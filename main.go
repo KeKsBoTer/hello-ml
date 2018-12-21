5fbb886e475009776c09a68c2db323714836c558
KeKsBoTer <14186588+KeKsBoTer@users.noreply.github.com>
2018-12-21 17:45:08 +0100

    added learning

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"math"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/KeKsBoTer/hello-ml/decoder"
 )
 
 func main() {
+
+	mode := os.Args[1]
+
 	rand.Seed(time.Now().UnixNano())
 	images, err := decoder.DecodeData("data/images.data", "data/labels.data")
 
@@ -17,10 +22,55 @@ func main() {
 		panic(err)
 	}
 	nn := NewNN(28*28, 16, 16, 10)
-	nn.Randomize()
 
-	out := nn.runBatch(images)
-	fmt.Println("Cost: ", out)
+	b, err := ioutil.ReadFile("models/model_1000.gob")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	nn.GobDecode(b)
+
+	switch mode {
+	case "train":
+		fmt.Println("runing 10000 epochs")
+		for i := 0; i < 10000; i++ {
+			out := nn.runBatch((*images)[0:100], 0.1)
+			fmt.Printf("Step %d, Cost:  %f\n", i, out)
+			perm := rand.Perm(len(*images))
+			for i, v := range perm {
+				(*images)[v], (*images)[i] = (*images)[i], (*images)[v]
+			}
+			if i%1000 == 0 {
+				data, err := nn.GobEncode()
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					err := ioutil.WriteFile(fmt.Sprintf("models/model_%d.gob", i), data, 0x666)
+					if err != nil {
+						fmt.Println(err)
+					}
+				}
+			}
+		}
+	case "test":
+		fmt.Println("running 100 random test numbers")
+		accuracy := make([]int, 100)
+		for i := 0; i < 100; i++ {
+			guess := nn.GuessNumber((*images)[i].Image)
+			fmt.Printf("image is a %d, guessed %d\n", (*images)[i].Label, guess)
+			if uint8(guess) == (*images)[i].Label {
+				accuracy[i] = 1
+			}
+		}
+		sum := 0
+		for _, v := range accuracy {
+			sum += v
+		}
+		fmt.Println("accuracy:", float64(sum)/100)
+	default:
+		fmt.Println("unknown mode. run with 'train' or 'test' as argument")
+		return
+	}
 }
 
 func sigmoid(x float64) float64 {
--- nn.go
+++ nn.go
@@ -52,67 +52,121 @@ func (n *NeuronalNetwork) GobDecode(buf []byte) (err error) {
 }
 
 // GuessNumber guesses the number in the image
-func (n *NeuronalNetwork) GuessNumber(image *decoder.LabeledImage) int {
-	return n.process(image.Image).Max()
+func (n *NeuronalNetwork) GuessNumber(image num.Vector) int {
+	activations := n.process(image)
+	return activations[len(activations)-1].Max()
 }
 
-func (n *NeuronalNetwork) process(input num.Vector) *num.Vector {
+func (n *NeuronalNetwork) process(input num.Vector) []num.Vector {
+	activations := make([]num.Vector, len(n.layers)+1)
+	activations[0] = input
 	last := input
-	for _, l := range n.layers {
+	for i, l := range n.layers {
 		processed := l.process(last)
 		last = *processed.Apply(sigmoid)
+		activations[i+1] = last
 	}
-	return &last
+	return activations
 }
 
-func (n *NeuronalNetwork) runBatch(input *[]decoder.LabeledImage) float64 {
+func (n *NeuronalNetwork) runBatch(input []decoder.LabeledImage, learnFactor float64) float64 {
 	avgCost := 0.0
-	for i := 0; i < len(*input); i++ {
-		sample := (*input)[i]
-		result := n.process(sample.Image)
+	var avgGradient []Layer
+	for i := 0; i < len(input); i++ {
+		sample := input[i]
+		activations := n.process(sample.Image)
+		result := activations[len(activations)-1]
 		// create expected result (1 for expected number and 0 for rest)
-		expected := make([]float64, len(*result))
+		expected := make([]float64, len(result))
 		expected[sample.Label] = 1
 		avgCost += result.Cost(expected)
-		n.backprop(*result, expected)
+		gradient := n.backprop(activations, expected)
+		if i == 0 {
+			avgGradient = gradient
+		} else {
+			for layer := range avgGradient {
+				for k := range avgGradient[layer].Biasas {
+					avgGradient[layer].Biasas[k] = (avgGradient[layer].Biasas[k]*float64(i) + gradient[layer].Biasas[k]) / (float64(i) + 1)
+				}
+				for k := range avgGradient[layer].Weights {
+					for l := range avgGradient[layer].Weights[k] {
+						avgGradient[layer].Weights[k][l] = (avgGradient[layer].Weights[k][l]*float64(i) + gradient[layer].Weights[k][l]) / (float64(i) + 1)
+					}
+				}
+			}
+		}
 	}
-	return avgCost / float64(len(*input))
+	n.applyGradient(avgGradient, learnFactor)
+
+	return avgCost / float64(len(input))
 }
 
-func (n *NeuronalNetwork) backprop(out, expected num.Vector) {
-	/*
-		nabla_b := make([]num.Vector, len(n.layers))
-		nabla_w := make([]num.Vector, len(n.layers))
-		for i := range nabla_b {
-			nabla_b[i] = *num.NewVector(len(n.layers[i].Biasas))
-			nabla_w[i] = *num.NewVector(len(n.layers[i].Weights))
+func (n *NeuronalNetwork) backprop(activations []num.Vector, expected num.Vector) []Layer {
+	costGradient := n.emptyLayers()
+	// derivation of cost to previous activations
+	d_pa := *num.NewVector(len(expected))
+	lastLayer := activations[len(activations)-1]
+	for j := range d_pa {
+		d_pa[j] = 2 * (lastLayer[j] - expected[j])
+	}
+	for L := len(n.layers) - 1; L >= 0; L-- {
+		layer := n.layers[L]
+		for j := range layer.Biasas { // amout of biasas is amount of neurons
+			// calculate z of neuron j in layer L
+			zLj := n.calcZ(activations, L, j)
+			// calculate derivative of bias j in layer L to cost
+			d_bLj := sigmoidDerivative(zLj) * d_pa[j]
+			costGradient[L].Biasas[j] = d_bLj
+
+			// calc weights
+			for k := range layer.Weights[j] {
+				d_wLjk := activations[L /*-1*/][k] * sigmoidDerivative(zLj) * d_pa[j]
+				costGradient[L].Weights[j][k] = d_wLjk
+			}
 		}
-
-		activation := out
-		activations := []num.Vector{out}
-		var zs []*num.Vector
-
-		for _, l := range n.layers {
-			z := l.process(activation)
-			zs = append(zs, z)
-			activation = *z.Apply(sigmoid)
-			activations = append(activations, activation)
+		// previous Layer
+		newd_pa := *num.NewVector(len(activations[L]))
+		for k := range newd_pa {
+			dalm1k := 0.0
+			for j := range layer.Biasas {
+				dalm1k += layer.Weights[j][k] * sigmoidDerivative(n.calcZ(activations, L, j)) * d_pa[j]
+			}
+			newd_pa[k] = dalm1k
 		}
-
-		costs := *n.costDerivative(activations[len(activations)-1], expected)
-		sigmoids := *zs[len(zs)-1].Apply(sigmoidDerivative)
-		delta := *num.NewVector(len(costs))
-		for i := range delta {
-			delta[i] = costs[i] * sigmoids[i]
-		}
-
-		nabla_b[len(nabla_b)-1] = delta
-		nabla_w[len(nabla_w)-1] = *delta.Dot(activations[len(activations)-2])
-	*/
+		d_pa = newd_pa
+	}
+	return costGradient
 }
 
-func (n *NeuronalNetwork) costDerivative(out, y num.Vector) *num.Vector {
-	return &out
+func (n *NeuronalNetwork) applyGradient(gradient []Layer, learnFactor float64) {
+	for layer := range gradient {
+		for k := range gradient[layer].Biasas {
+			n.layers[layer].Biasas[k] -= gradient[layer].Biasas[k] * learnFactor
+		}
+		for k := range gradient[layer].Weights {
+			for l := range gradient[layer].Weights[k] {
+				n.layers[layer].Weights[k][l] -= gradient[layer].Weights[k][l] * learnFactor
+			}
+		}
+	}
+}
+
+func (n *NeuronalNetwork) calcZ(activations []num.Vector, L int, j int) float64 {
+	layer := n.layers[L]
+	z := layer.Biasas[j]
+	for i := range layer.Weights[j] {
+		z += layer.Weights[j][i] * activations[L /*-1*/][i]
+	}
+	return z
+}
+
+func (n *NeuronalNetwork) emptyLayers() []Layer {
+	empty := make([]Layer, len(n.layers))
+	for i := range empty {
+		empty[i].Biasas = *num.NewVector(len(n.layers[i].Biasas))
+		empty[i].Weights = *num.NewMat(len(n.layers[i].Weights), len(n.layers[i].Weights[0]))
+	}
+	return empty
 }
 
 // VectorToImage takes a vector and turns it into a image with the given width
